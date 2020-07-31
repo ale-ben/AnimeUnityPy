17b502e45f65d3684fbc6d440ad12b75cb34b6aa
Alessandro Benetton MBP <aleben98@gmail.com>
2020-07-31 11:40:57 +0200

    First step

---
+++ QT_Creator/ui-loader.py
@@ -0,0 +1,24 @@
+import sys
+
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QPushButton, QLineEdit
+from PySide2.QtCore import QFile, QObject
+
+
+class Form(QObject):
+
+    def __init__(self, ui_file, parent=None):
+        super(Form, self).__init__(parent)
+        ui_file = QFile(ui_file)
+        ui_file.open(QFile.ReadOnly)
+
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        ui_file.close()
+        self.window.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = Form('searchwindow.ui')
+    sys.exit(app.exec_())
---
+++ gui.py
@@ -0,0 +1,58 @@
+import sys
+
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QPushButton, QLineEdit, QComboBox
+from PySide2.QtCore import QFile, QObject, Signal, Slot
+
+search_ui = 'UIs/searchwindow.ui'
+
+
+class SearchWindow(QObject):
+    search_res_signal = Signal(dict)
+
+    def __init__(self):
+        super(SearchWindow, self).__init__()
+
+        ui_file = QFile(search_ui)
+
+        if not ui_file.open(QFile.ReadOnly):
+            print("Cannot open {}: {}".format(search_ui, ui_file.errorString()))
+            sys.exit(-1)
+
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        self.title_ledit = self.window.findChild(QLineEdit, 'title_ledit')
+        self.genre_cbox = self.window.findChild(QComboBox, 'genre_cbox')
+        self.year_ledit = self.window.findChild(QLineEdit, 'year_ledit')
+        self.search_btn = self.window.findChild(QPushButton, 'search_btn')
+
+        ui_file.close()
+
+        self.bind_attributes()
+        self.window.show()
+
+    def bind_attributes(self):
+        self.search_btn.clicked.connect(self.debug_print)
+
+    def debug_print(self):
+        title = self.year_ledit.text()
+        genre = self.genre_cbox.currentText()
+        year = self.year_ledit.text()
+        print(f"|{title}|{genre}|{year}")
+        self.search_res_signal.emit({'title': title, 'genre': genre, 'year': year})
+
+
+@Slot(dict)
+def print_search_res(res):
+    print(res)
+
+
+def bind_ui():
+    search.search_res_signal.connect(print_search_res)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    search = SearchWindow()
+    bind_ui()
+    sys.exit(app.exec_())
